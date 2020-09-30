7638929716201268efa54de664ccdcdd68fec902
Andrew Bauer <andenrx@gmail.com>
2020-09-29 17:42:04 -0700

    Added base code for AddingMachine; does not do anything yet

---
+++ AddingMachine.java
@@ -0,0 +1,47 @@
+package cse360assignment02;
+
+public class AddingMachine {
+  private int total;
+
+  public AddingMachine () {
+  /**
+  * Creates a new AddingMachine object.
+  */
+    total = 0;  // not needed - included for clarity
+  }
+
+  public int getTotal () {
+  /**
+  * @return The value stored in the AddingMachine.
+  */
+    return 0;
+  }
+
+  public void add (int value) {
+  /**
+  * Add a value to the total and store the computation.
+  * @param  value the value to be added
+  */
+  }
+
+  public void subtract (int value) {
+  /**
+  * Add a value to the total and store the computation.
+  * @param  value the value to be added
+  */
+  }
+
+  public String toString () {
+  /**
+  * Formatted AddingMachine output. Displays all intermediate computations.
+  * @return String consisting of the computations being performed by the AddingMachine.
+  */
+    return "";
+  }
+
+  public void clear() {
+  /**
+  * Erase the history of the AddingMachine.
+  */
+  }
+}
